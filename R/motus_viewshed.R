b33378931ba154bf46ec374ba6dae58b1776b69c
Adam Smith <avianmigration@gmail.com>
2020-10-07 23:36:43 -0400

    Simplify to counter `rgdal` issues with WKT

--- R/motus_viewshed.R
+++ R/motus_viewshed.R
@@ -4,15 +4,13 @@ motus_viewshed <- function(coords = c(38.897659, -77.036564), ht = 0, zoom = 10)
   if (!requireNamespace("rgeos", quietly = TRUE)) install.packages("rgeos", quiet = TRUE)
   if (!requireNamespace("raster", quietly = TRUE)) install.packages("raster", quiet = TRUE)
   if (!requireNamespace("mapview", quietly = TRUE)) install.packages("mapview", quiet = TRUE)
-  
-  source("R/utils.R")
-  
+
   lon <- coords[2]; lat <- coords[1]
   coords <- cbind(lon, lat)
-  prj <- local_laea(lon, lat)
+  prj <- sprintf("+proj=laea +x_0=0 +y_0=0 +lon_0=%f +lat_0=%f", lon, lat)
   
   pt_ll <- sp::SpatialPoints(coords, sp::CRS("+init=epsg:4326"))
-  pt <- sp::spTransform(pt_ll, sp::CRS(SRS_string = prj))
+  pt <- sp::spTransform(pt_ll, sp::CRS(prj))
   poly15 <- rgeos::gBuffer(pt, width = 15000, quadsegs = 12)
   poly10 <- rgeos::gBuffer(pt, width = 10000, quadsegs = 12)
   poly5 <- rgeos::gBuffer(pt, width = 5000, quadsegs = 12)
@@ -31,21 +29,21 @@ motus_viewshed <- function(coords = c(38.897659, -77.036564), ht = 0, zoom = 10)
   
   # Set up display
   mt <- c("CartoDB.DarkMatter", "Esri.WorldImagery", "Esri.WorldTopoMap")
+  mapview::mapviewOptions(basemaps = mt, na.color = "#FFFFFF00")
   suppressWarnings(
-    m <- mapview::mapview(out, na.color = NA, layer.name = "Elev. deficit (m)",
-                          map.types = mt) +
+    m <- mapview::mapview(out, layer.name = "Elev. deficit (m)") +
       mapview::mapview(pt, layer.name = "Proposed station", 
-                       col.regions = NA, cex = 4, color = "orange",
-                       legend = FALSE, label = NULL, map.types = mt) +
+                       alpha.regions = 0, cex = 4, color = "orange",
+                       legend = FALSE, label = NULL) +
       mapview::mapview(poly15, layer.name = "15 km range", 
                        alpha.regions = 0, color = "white",
-                       legend = FALSE, label = NULL, map.types = mt) +
+                       legend = FALSE, label = NULL) +
       mapview::mapview(poly10, layer.name = "10 km range", 
                        alpha.regions = 0, color = "white",
-                       legend = FALSE, label = NULL, map.types = mt) +
+                       legend = FALSE, label = NULL) +
       mapview::mapview(poly5, layer.name = "5 km range", 
                        alpha.regions = 0, color = "white",
-                       legend = FALSE, label = NULL, map.types = mt)
+                       legend = FALSE, label = NULL)
   )
   m <- leaflet::setView(m@map, sp::coordinates(pt_ll)[1], 
                     sp::coordinates(pt_ll)[2],
