7a7b6cda0bbb10d9fc32e7534117e8e8653db1ff
Adam D. Smith <adam_d_smith@fws.gov>
2022-02-14 17:58:06 -0500

    All in sf; change color scheme to show lower defecits better

--- R/motus_viewshed.R
+++ R/motus_viewshed.R
@@ -1,7 +1,5 @@
 motus_viewshed <- function(coords = c(38.897659, -77.036564), ht = 0, zoom = 10) {
   if (!requireNamespace("elevatr", quietly = TRUE)) install.packages("elevatr", quiet = TRUE)
-  if (!requireNamespace("sp", quietly = TRUE)) install.packages("sp", quiet = TRUE)
-  if (!requireNamespace("rgeos", quietly = TRUE)) install.packages("rgeos", quiet = TRUE)
   if (!requireNamespace("raster", quietly = TRUE)) install.packages("raster", quiet = TRUE)
   if (!requireNamespace("mapview", quietly = TRUE)) install.packages("mapview", quiet = TRUE)
 
@@ -11,27 +9,25 @@ motus_viewshed <- function(coords = c(38.897659, -77.036564), ht = 0, zoom = 10)
   
   # Calculate range rings
   lon <- coords[2]; lat <- coords[1]
-  coords <- cbind(lon, lat)
+  # coords <- cbind(lon, lat)
   prj <- sprintf("+proj=laea +x_0=0 +y_0=0 +lon_0=%f +lat_0=%f", lon, lat)
   
-  pt_ll <- sp::SpatialPoints(coords, sp::CRS("+init=epsg:4326"))
-  pt <- sp::spTransform(pt_ll, sp::CRS(prj))
-  poly20 <- rgeos::gBuffer(pt, width = 20000, quadsegs = 12)
-  poly15 <- rgeos::gBuffer(pt, width = 15000, quadsegs = 12)
-  poly10 <- rgeos::gBuffer(pt, width = 10000, quadsegs = 12)
-  poly5 <- rgeos::gBuffer(pt, width = 5000, quadsegs = 12)
+  pt_ll <- sf::st_sfc(sf::st_point(rev(coords)), crs = 4326)
+  pt <- sf::st_transform(pt_ll, prj)
+  poly20 <- sf::st_buffer(pt, 20000)
+  poly15 <- sf::st_buffer(pt, 15000)
+  poly10 <- sf::st_buffer(pt, 10000)
+  poly5 <- sf::st_buffer(pt, 5000)
   divisions <- st_wedges(0, 0, 20000, 8)
   divisions <- sf::st_sf(sf::st_set_crs(divisions, prj))
   
   # Get DEM
-  suppressWarnings(
-    suppressMessages(
+  suppressMessages(
       elev <- elevatr::get_elev_raster(poly20, z = zoom, clip = "locations", verbose = FALSE)
-    )
   )
 
   # Calculate viewshed
-  pt_elev <- raster::extract(elev, pt) + ht
+  pt_elev <- raster::extract(elev, sf::as_Spatial(pt)) + ht
   
   if (all(pt_elev > max(raster::values(elev), na.rm = TRUE))) {
     message("No elevation deficits found in viewshed. Displaying only 5, 10, and 15 km range rings.")
@@ -40,42 +36,42 @@ motus_viewshed <- function(coords = c(38.897659, -77.036564), ht = 0, zoom = 10)
                             alpha.regions = 0, cex = 4, color = "orange",
                             legend = FALSE, label = NULL) +
         mapview::mapview(poly15, layer.name = "15 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly10, layer.name = "10 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly5, layer.name = "5 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL))
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL))
   } else {
     out <- elev - pt_elev
     out[out < 0] <- NA
     suppressWarnings(
-      m <- mapview::mapview(out, layer.name = "Elev. deficit (m)") +
+      m <- mapview::mapview(out, layer.name = "Elev. deficit (m)",
+                            col.regions = grDevices::hcl.colors(12, palette = "viridis")) +
         mapview::mapview(pt, layer.name = "Proposed station", 
                          alpha.regions = 0, cex = 4, color = "orange",
-                         legend = FALSE, label = NULL) +
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(divisions, layer.name = "Direction guide",
-                        alpha.regions = 0, color = "gray50",
-                        legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly20, layer.name = "20 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly15, layer.name = "15 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly10, layer.name = "10 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL) +
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL) +
         mapview::mapview(poly5, layer.name = "5 km range", 
-                         alpha.regions = 0, color = "gray50",
-                         legend = FALSE, label = NULL))
+                         alpha.regions = 0, color = "white",
+                         lwd = 1.25, legend = FALSE, label = NULL))
   }
 
-  m <- leaflet::setView(m@map, sp::coordinates(pt_ll)[1], 
-                        sp::coordinates(pt_ll)[2],
-                        zoom = 11)
+  
+  # m <- leaflet::setView(m@map, lon, lat, zoom = 11)
   return(m)
 }
 
